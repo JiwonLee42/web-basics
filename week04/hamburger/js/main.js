4aa81196146e018d326c42635d2ccda2dcc99286
JiwonLee <ljw56b@gmail.com>
2021-04-12 03:16:54 +0900

    clone coding
    header,menu

---
+++ week04/hamburger/js/main.js
@@ -0,0 +1,16 @@
+// 햄버거 바 아이콘 선택자
+const menuOpenEL= document.getElementById('btn--open')
+// 닫는 아이콘 선택자
+const menuOpenEL= document.getElementById('btn--close')
+//사이드 메뉴 선택자
+const sideMenuEL = document.getElementById.By.Id('nav_menu')
+
+menuOpenEl.addEventListener("click", function() {
+    menuOpenEL.style.display-"none"
+    menuOpenEL.style.display-"block"
+})
+
+menuCloseEl.addEventListener("click", function() {
+    menuOpenEL.style.display-"block"
+    menuCloseEL.style.display-"none"
+})
