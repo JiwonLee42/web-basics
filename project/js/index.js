149098037100d37a4a6ff3606d7aa7a0022f5401
JiwonLee <ljw56b@gmail.com>
2021-04-15 02:00:10 +0900

    clone coding
    main

---
+++ project/js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+    var small = $('.small');
+    small.addClass('bigimg');
+  });
+
+  $(function(){
+  var container = $(".small image"); 
+  var slideShow = container.find(".small image");
+  var slideImg = slideShow.find("img");
+  var slides = slideImg.find(">div");      
+
+  var slideBtn = container.find("slide_button")});
