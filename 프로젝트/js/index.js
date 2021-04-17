87b21d5d7cdfc5bb69a9455c44a6c52bd9fa5b53
JiwonLee <ljw56b@gmail.com>
2021-04-17 16:33:24 +0900

    clone coding
    reset..

---
+++ 프로젝트/js/index.js
@@ -0,0 +1,4 @@
+//const webtoon = Document.querySelector(".webtoon"); //제일 처음의 class를 제거해주기
+webtoon.addEventListener('click', () => {   //클릭하면 newwebtoon class를 가지도록 하기
+    webtoon.classList.add("newwebtoon");
+}) 
